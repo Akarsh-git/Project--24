7e90690ececdaba0d79137d1ed0234d5a1350115
Akarsh-git <75009327+Akarsh-git@users.noreply.github.com>
2021-12-15 17:38:38 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -18,9 +18,9 @@ function setup() {
 
 	ground = new Ground(400,690,800,20);
 	box1 = new Box(600,680,200,20);
-	box2 = new Box(600-100,600,15,160);
+	box2 = new Box(600-100,680,15,160);
 	box3 = new Box(600+100,680,15,160);
-	paper = new Paper(100,100)
+	paper = new Paper(100,100,20)
 
 
 	Engine.run(engine);
@@ -40,5 +40,10 @@ function draw() {
  
 }
 
-
+function keyPressed()
+ {
+	  if (keyCode === UP_ARROW) { 
+		  Matter.Body.applyForce(paper.body,paper.body.position,{x:75,y:-45}); 
+		}
+	 }
 
